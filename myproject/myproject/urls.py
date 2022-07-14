9c902e53e4c281942cf77504dab321d40ee3b5fa
sanheekim <68312957+sanheekim@users.noreply.github.com>
2022-07-14 23:00:51 +0900

    url 연결

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+from myproject.polls import url
+
+"""최상위 url 작성"""
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls'))
 ]
---
+++ myproject/polls/url.py
@@ -0,0 +1,7 @@
+from django.urls import URLPattern, path
+
+from . import views
+
+URLPatterns = [
+    path('', views.index, name='index'),
+    ]
