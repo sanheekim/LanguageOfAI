710fbec018513b8f4ae3a812daa48ab651ce25a1
sanheekim <68312957+sanheekim@users.noreply.github.com>
2022-08-03 13:46:44 +0900

    제너릭 뷰 사용하기 (오류 해결 중)

--- myproject/polls/urls.py
+++ myproject/polls/urls.py
@@ -6,13 +6,21 @@ from . import views
 app_name = 'polls' # myproject/urls.py에 namespace = 'polls' 추가할 때 필요함.
 urlpatterns = [
     # ex: /polls/
-    path('',views.index, name='index'),
+    path('',views.IndexView.as_view(), name='index'),
     # ex: /polls/5
-    #path('<int:question_id>/',views.detail,name='detail'),
     #위 url에 /specifics/를 추가
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+''' 제너릭 뷰 사용하기 전 코드
+    # path('',views.index, name='index'),
+    #path('<int:question_id>/',views.detail,name='detail'),
+     # path('<int:question_id>/detail/', views.detail, name='results'),
+    # path('<int:question_id>/results/', views.results, name='results'),
+
+    # 제너릭 뷰 사용법 : polls/views.py에서 참고
+'''
--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader 
-
+from django.template import loader
 from .models import Question, Choice
+from django.views import generic # 제너릭 뷰 사용
 
 """
 템플릿에 context 를 채워넣어 표현한 결과를 HttpResponse 객체와 함께 돌려주는 구문을
@@ -22,14 +22,14 @@ from django.shortcuts import render, get_object_or_404
     return HttpResponse(output) """
 
  # 질문이 여러 개인 경우 template 활용
-def index(request):
+'''def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]    
     template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
     # return HttpResponse(template.render(context,request))
-    return render(request, 'polls/index.html', context)
+    return render(request, 'polls/index.html', context)'''
 
 # 404 에러 일으키기 (존재하지 않는 경로에 들어갔을 때 지정한 문구가 뜨도록 함)
 '''from django.http import Http404
@@ -45,9 +45,9 @@ def detail(request, question_id):
 
 # 404 에러 지름길: get_object_or_404()
 # from django.shortcuts import get_object_or_404, render
-def detail(request, question_id):
+'''def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    return render(request, 'polls/detail.html', {'question': question})'''
 
 '''
 def results(request, question_id):
@@ -55,9 +55,10 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 '''
 
-def results(request, question_id):
+#  라디오박스 값 넘기기
+'''def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question':question})
+    return render(request, 'polls/results.html', {'question':question})'''
 
 from django.urls import reverse
 
@@ -78,3 +79,23 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+        # 아래부터는 제너릭 뷰 사용법
+        class IndexView(generic.ListView):
+            template_name = 'polls/index.html'
+            context_object_name = 'latest_question_list'
+
+            def get_queryset(self):
+                """Return the last five published questions."""
+                return Question.objects.order_by('-pub_date')[:5]
+
+
+        class DetailView(generic.DetailView):
+            model = Question
+            template_name = 'polls/detail.html'
+
+
+        class ResultsView(generic.DetailView):
+            model = Question
+            template_name = 'polls/results.html'
