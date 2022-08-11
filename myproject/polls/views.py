3c8d0e16af489421f5a5024656a85e81e9d95b3b
sanheekim <sanheekim526@gmail.com>
2022-08-11 18:45:35 +0900

    뷰 개선시키기

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -80,6 +80,7 @@ def vote(request, question_id):
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
+from django.utils import timezone
 
 # 아래부터는 제너릭 뷰 사용법
 class IndexView(generic.ListView):
@@ -87,8 +88,13 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+            """
+            Return the last five published questions (not including those set to be
+            published in the future).
+            """
+            return Question.objects.filter(
+                pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
