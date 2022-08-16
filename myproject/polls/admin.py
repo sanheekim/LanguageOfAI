862ee7081483822ed751a492b7d3ff32d96b2660
sanheekim <sanheekim526@gmail.com>
2022-08-16 19:07:29 +0900

    관리자 폼 커스터마이징

--- myproject/polls/admin.py
+++ myproject/polls/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Choice, Question
 
-admin.site.register(Question)
+# 관리자 폼 커스터마이징(8/16)
+class QuestionAdmin(admin.ModelAdmin):
+     fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
